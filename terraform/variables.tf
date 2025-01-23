742137ca4ca3eb7a1639b912218832a339c5f469
7174Andy <hep003@ucsd.edu>
2025-01-23 14:58:49 -0800

    add new variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,9 @@ variable "project_id" {
   type        = string
   default     = "vmassign-dev"
 }
+
+variable "resource_suffix" {
+  description = "Suffix to append to all resources"
+  type        = string
+  default     = "prod"
+}
