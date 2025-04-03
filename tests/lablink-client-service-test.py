0a5ffa4e6e6ddee6c5570045a093bcb57b23bae6
7174Andy <hep003@ucsd.edu>
2025-04-03 13:46:24 -0700

    add tests

---
+++ tests/lablink-client-service-test.py
@@ -0,0 +1,8 @@
+"""Module that tests the imports of the package."""
+
+
+def test_import():
+    import lablink_client_service
+
+    from lablink_client_service import subscribe
+    from lablink_client_service import database
