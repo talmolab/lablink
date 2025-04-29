a51bc4c18a6af7af57b2fbaf034b45b29d131cec
Andrew Park <hep003@ucsd.edu>
2025-04-29 16:15:45 -0700

    Hide Sensitive Content

--- lablink-client-base/lablink-client-base-image/start.sh
+++ lablink-client-base/lablink-client-base-image/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "Running subscribe script..."
 # Activate the conda environment and run the subscribe script
-/home/client/miniforge3/bin/conda run -n base subscribe
+/home/client/miniforge3/bin/conda run -n base subscribe db.host=$DB_HOST
