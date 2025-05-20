d5da83382621569a812f46305bc6a71c18c35bcd
7174Andy <hep003@ucsd.edu>
2025-05-20 11:45:46 -0700

    client image sleep indefinitely

--- lablink-client-base/lablink-client-base-image/start.sh
+++ lablink-client-base/lablink-client-base-image/start.sh
@@ -4,3 +4,6 @@ echo "Running subscribe script..."
 
 # Activate the conda environment and run the subscribe script
 /home/client/miniforge3/bin/conda run -n base subscribe allocator.host=$ALLOCATOR_HOST
+
+# Keep the container alive
+tail -f /dev/null
