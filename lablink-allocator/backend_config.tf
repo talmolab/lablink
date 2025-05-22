f6ff872c2e8b0fb5a58ba6692bd328447f79df5c
7174Andy <hep003@ucsd.edu>
2025-05-22 14:50:53 -0700

    change region

--- lablink-allocator/backend_config.tf
+++ lablink-allocator/backend_config.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tf-state-bucket"
     key            = "lablink/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-west-1"
     dynamodb_table = "lock-table"
     encrypt        = true
   }
