6acc7eaf4b3c292be3173522f65c63388b1e4d0a
7174Andy <hep003@ucsd.edu>
2025-05-28 22:36:00 -0700

    remove db_config.py

--- lablink-allocator/lablink-allocator-service/get_cofig.py
+++ lablink-allocator/lablink-allocator-service/get_cofig.py
@@ -2,6 +2,7 @@ from hydra import initialize, compose
 from omegaconf import OmegaConf
 from conf.structured_config import Config
 
+
 def get_config() -> Config:
     """
     Load the configuration file using Hydra and return it as a dictionary.
