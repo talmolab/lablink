bc260525346e0db999dac7054f9436722bf2ad64
7174Andy <hep003@ucsd.edu>
2025-05-28 22:48:51 -0700

    fix bug

--- db_config.py
+++
@@ -1,6 +0,0 @@
-DB_NAME = "lablink_db"
-DB_USER = "lablink"
-DB_PASSWORD = "lablink"
-DB_PORT = 5432
-VM_TABLE = "vms"
-MESSAGE_CHANNEL = "vm_updates"
--- lablink-allocator/lablink-allocator-service/conf/structured_config.py
+++ lablink-allocator/lablink-allocator-service/conf/structured_config.py
@@ -3,6 +3,7 @@
 from hydra.core.config_store import ConfigStore
 from dataclasses import dataclass, field
 
+
 @dataclass
 class DatabaseConfig:
     dbname: str = field(default="lablink")
@@ -11,10 +12,13 @@ class DatabaseConfig:
     host: str = field(default="localhost")
     port: int = field(default=5432)
     table_name: str = field(default="vm_table")
-    
+    message_channel: str = field(default="vm_updates")
+
+
 @dataclass
 class Config:
     db: DatabaseConfig = field(default_factory=DatabaseConfig)
-    
+
+
 cs = ConfigStore.instance()
 cs.store(name="config", node=Config)
