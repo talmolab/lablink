23f1917c3a9a6cc9a93d763e9a38bedf39def329
7174Andy <hep003@ucsd.edu>
2025-06-01 14:38:22 -0700

    add logging for `get_config`

--- lablink-allocator/lablink-allocator-service/get_cofig.py
+++ lablink-allocator/lablink-allocator-service/get_cofig.py
@@ -1,6 +1,16 @@
 from hydra import initialize, compose
 from omegaconf import OmegaConf
 from conf.structured_config import Config
+import logging
+
+# Setup logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def get_config() -> Config:
