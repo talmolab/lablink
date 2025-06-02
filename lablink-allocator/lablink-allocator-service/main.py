737c09152b7d675b846e33bf5ab29f921814419a
7174Andy <hep003@ucsd.edu>
2025-06-02 11:31:17 -0700

    fix typo and clean up

--- lablink-allocator/lablink-allocator-service/main.py
+++ lablink-allocator/lablink-allocator-service/main.py
@@ -223,7 +223,7 @@ def launch():
         # Write the IP address to the terraform.tfvars file
         with open(os.path.join(terraform_dir, "terraform.runtime.tfvars"), "w") as f:
             f.write(f'allocator_ip = "{allocator_ip}"\n')
-            f.write(f'instance_type = "{vm_type}"\n')
+            f.write(f'machine_type = "{vm_type}"\n')
 
         # Apply with the new number of instances
         apply_cmd = [
