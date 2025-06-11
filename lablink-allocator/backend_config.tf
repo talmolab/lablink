359df944939a5297af33f43eaa5a0725840d9f13
eberrigan <berri104@gmail.com>
2025-06-10 17:19:16 -0700

    remove old backend

--- lablink-allocator/backend_config.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-lablink-allocator-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "lock-table"
-    encrypt        = true
-  }
-}
