3bb3a0f8f1351c6c2e330711840f0ce1598b144d
7174Andy <hep003@ucsd.edu>
2025-06-10 23:31:21 -0700

    fix error final

--- lablink-allocator/main.tf
+++ lablink-allocator/main.tf
@@ -38,6 +38,7 @@ resource "aws_instance" "lablink_allocator_server" {
               #!/bin/bash
               apt update -y
               apt install -y docker.io
+              apt install -y rsync
               systemctl start docker
               systemctl enable docker
               docker login ghcr.io -u USERNAME -p GITHUB_TOKEN
