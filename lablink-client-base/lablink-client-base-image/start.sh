2a90deb44e5376d87d5953a22ccd2501e4fef194
7174Andy <hep003@ucsd.edu>
2025-07-02 13:51:59 -0700

    change port

--- lablink-client-base/lablink-client-base-image/start.sh
+++ lablink-client-base/lablink-client-base-image/start.sh
@@ -3,7 +3,7 @@
 echo "Running subscribe script..."
 
 # Activate the conda environment and run the subscribe script
-/home/client/miniforge3/bin/conda run -n base subscribe allocator.host=$ALLOCATOR_HOST
+/home/client/miniforge3/bin/conda run -n base subscribe allocator.host=$ALLOCATOR_HOST allocator.port=80
 
 # Keep the container alive
 tail -f /dev/null
