f1983f0ad767e7f871ed92d71173235ca6069919
7174Andy <hep003@ucsd.edu>
2025-07-08 09:54:06 -0700

    remove pid

--- lablink-client-base/lablink-client-base-image/start.sh
+++ lablink-client-base/lablink-client-base-image/start.sh
@@ -24,7 +24,7 @@ fi
 
 # Activate the conda environment and run the subscribe script
 /home/client/miniforge3/bin/conda run -n base subscribe allocator.host=$ALLOCATOR_HOST allocator.port=80 client.software=$SUBJECT_SOFTWARE
-subscribe_pid=$!
+
 
 # Wait for the subscribe script to start
 sleep 5
