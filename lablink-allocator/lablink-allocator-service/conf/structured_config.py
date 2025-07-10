2d7c28647c56aad0df1587d833e9c1fc99fcd43b
7174Andy <hep003@ucsd.edu>
2025-07-10 15:29:58 -0700

    add docstring

--- lablink-allocator/lablink-allocator-service/conf/structured_config.py
+++ lablink-allocator/lablink-allocator-service/conf/structured_config.py
@@ -8,6 +8,20 @@ from hydra.core.config_store import ConfigStore
 
 @dataclass
 class DatabaseConfig:
+    """Configuration for the database used in the LabLink Allocator Service.
+    This class defines the connection parameters for the database, including the name, user,
+    password, host, port, table name, and message channel.
+
+    Attributes:
+        dbname (str): The name of the database.
+        user (str): The username for the database.
+        password (str): The password for the database.
+        host (str): The host where the database is located.
+        port (int): The port on which the database is running.
+        table_name (str): The name of the table to store VM information.
+        message_channel (str): The name of the message channel for updates.
+    """
+
     dbname: str = field(default="lablink")
     user: str = field(default="lablink")
     password: str = field(default="lablink_password")
@@ -19,12 +33,30 @@ class DatabaseConfig:
 
 @dataclass
 class AppConfig:
+    """Configuration for the LabLink Allocator Service application.
+
+    Attributes:
+        admin_user (str): The username for the admin user.
+        admin_password (str): The password for the admin user.
+    """
+
     admin_user: str = field(default="admin")
     admin_password: str = field(default="admin_password")
 
 
 @dataclass
 class MachineConfig:
+    """Configuration for the machine used in the LabLink Allocator Service.
+    This class defines the machine type, repository, image, AMI ID, and software to be used.
+
+    Attributes:
+        machine_type (str): The type of the machine to be used.
+        repository (Optional[str]): The repository URL for the machine image.
+        image (str): The Docker image ID to be used for the machine.
+        ami_id (str): The Amazon Machine Image (AMI) ID for the machine.
+        software (str): The software to be installed on the machine.
+    """
+
     machine_type: str = field(default="g4dn.xlarge")
     repository: Optional[str] = field(default=None)
     image: str = field(default="ghcr.io/talmolab/lablink-client-base-image:latest")
@@ -34,6 +66,15 @@ class MachineConfig:
 
 @dataclass
 class Config:
+    """Configuration for the LabLink Allocator Service.
+    This class aggregates the database, machine, and application configurations.
+
+    Attributes:
+        db (DatabaseConfig): The database configuration.
+        machine (MachineConfig): The machine configuration.
+        app (AppConfig): The application configuration.
+    """
+
     db: DatabaseConfig = field(default_factory=DatabaseConfig)
     machine: MachineConfig = field(default_factory=MachineConfig)
     app: AppConfig = field(default_factory=AppConfig)
