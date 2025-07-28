f7cd67728ad0f1f2c7f528b303fc154705b8fa84
7174Andy <hep003@ucsd.edu>
2025-07-28 10:58:05 -0700

    remove repo check

--- lablink-allocator/lablink-allocator-service/terraform/user_data.sh
+++ lablink-allocator/lablink-allocator-service/terraform/user_data.sh
@@ -70,21 +70,11 @@ if [ "$HAS_GPU" = true ]; then
 fi
 
 echo ">> Starting container..."
-if [ -z "${repository:-}" ]; then
-    echo ">> No repo specified; starting container without cloning."
-    docker run -dit $DOCKER_GPU_ARGS \
-        -e ALLOCATOR_HOST="${allocator_ip}" \
-        -e VM_NAME="lablink-vm-${resource_suffix}-${count_index}" \
-        -e SUBJECT_SOFTWARE="${subject_software}" \
-        "${image_name}"
-else
-    echo ">> Cloning repo and starting container."
-    docker run -dit $DOCKER_GPU_ARGS \
-        -e ALLOCATOR_HOST="${allocator_ip}" \
-        -e TUTORIAL_REPO_TO_CLONE="${repository}" \
-        -e VM_NAME="lablink-vm-${resource_suffix}-${count_index}" \
-        -e SUBJECT_SOFTWARE="${subject_software}" \
-        "${image_name}"
-fi
+docker run -dit $DOCKER_GPU_ARGS \
+    -e ALLOCATOR_HOST="${allocator_ip}" \
+    -e TUTORIAL_REPO_TO_CLONE="${repository}" \
+    -e VM_NAME="lablink-vm-${resource_suffix}-${count_index}" \
+    -e SUBJECT_SOFTWARE="${subject_software}" \
+    "${image_name}"
 
 echo ">> Container launched."
