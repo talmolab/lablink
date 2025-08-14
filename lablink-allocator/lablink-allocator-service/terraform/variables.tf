dd554de1cba6c2049488a3b96382f1498fdabc33
7174Andy <hep003@ucsd.edu>
2025-08-14 09:26:00 -0700

    remove vars for aws credentials

--- lablink-allocator/lablink-allocator-service/terraform/variables.tf
+++ lablink-allocator/lablink-allocator-service/terraform/variables.tf
@@ -3,24 +3,6 @@ variable "instance_count" {
   default = 1
 }
 
-variable "aws_access_key" {
-  type        = string
-  description = "AWS Access Key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS Secret Key"
-  sensitive   = true
-}
-
-variable "aws_session_token" {
-  type        = string
-  description = "AWS Session Token"
-  sensitive   = true
-}
-
 variable "allocator_ip" {
   type        = string
   description = "IP address of the allocator server"
