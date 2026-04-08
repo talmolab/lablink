8f8e1fae63f7202d68b6b8d8f0424c93980d4576
7174Andy <hep003@ucsd.edu>
2026-04-07 22:44:23 -0700

    chore: bump terraform package

--- packages/cli/src/lablink_cli/__init__.py
+++ packages/cli/src/lablink_cli/__init__.py
@@ -1,8 +1,8 @@
 """LabLink CLI - Deploy and manage LabLink infrastructure."""
 
 TEMPLATE_REPO = "talmolab/lablink-template"
-TEMPLATE_VERSION = "v0.1.0"
+TEMPLATE_VERSION = "v0.1.1"
 # SHA-256 of the GitHub release tarball for TEMPLATE_VERSION.
 # Update this when bumping TEMPLATE_VERSION.
 # To compute: curl -sL <tarball_url> | sha256sum
-TEMPLATE_SHA256 = "b53d00b9a580e38a9e4bd7d4cec1505846d21900a382dbb3a58f72f711347e3c"
+TEMPLATE_SHA256 = "c1d3d31ed318ea22c9ba59450779caf071b0d37af8e806ed4ee837811347311a"
